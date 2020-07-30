5103bf42132deb89a7ac8d03277886e3edd281c2
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-07-30 10:17:09 -0700

    remove marks from helpers

--- src/components/Helpers.js
+++ src/components/Helpers.js
@@ -9,29 +9,6 @@ export const useStyles = makeStyles(() => ({
     }
 }));
 
-export function valueText(value) {
-    return `${value}`;
-  }
-
-  export const marks = [
-	{
-		value: 1500,
-		label: 'very slow',
-	},
-	{
-		value: 1000,
-		label: 'slow',
-	},
-	{
-		value: 500,
-		label: 'med',
-	},
-	{
-		value: 75,
-		label: 'fast',
-	},
-];
-
 
 export const countPeeps = [
 	[0, 1],
